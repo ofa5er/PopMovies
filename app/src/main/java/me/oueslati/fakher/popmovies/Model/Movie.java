23e529a7e870f94489de6fd338ec216f138c3b64
Fakher Oueslati <oueslati.fakher@gmail.com>
2017-03-09 21:52:06 -0500

    feat: Added Movie Model

---
+++ app/src/main/java/me/oueslati/fakher/popmovies/Model/Movie.java
@@ -0,0 +1,145 @@
+package me.oueslati.fakher.popmovies.Model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Movie {
+    
+    private String posterPath;
+    private Boolean adult;
+    private String overview;
+    private String releaseDate;
+    private List<Object> genreIds = null;
+    private Integer id;
+    private String originalTitle;
+    private String originalLanguage;
+    private String title;
+    private String backdropPath;
+    private Double popularity;
+    private Integer voteCount;
+    private Boolean video;
+    private Integer voteAverage;
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public Boolean getAdult() {
+        return adult;
+    }
+
+    public void setAdult(Boolean adult) {
+        this.adult = adult;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public List<Object> getGenreIds() {
+        return genreIds;
+    }
+
+    public void setGenreIds(List<Object> genreIds) {
+        this.genreIds = genreIds;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    public void setOriginalLanguage(String originalLanguage) {
+        this.originalLanguage = originalLanguage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public Double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(Double popularity) {
+        this.popularity = popularity;
+    }
+
+    public Integer getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(Integer voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public Boolean getVideo() {
+        return video;
+    }
+
+    public void setVideo(Boolean video) {
+        this.video = video;
+    }
+
+    public Integer getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(Integer voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+}
